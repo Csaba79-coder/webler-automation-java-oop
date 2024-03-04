781bd7a5b4ca291d1c7db44b8aca26f67f57047b
Csaba Vadász <csabavadasz79@gmail.com>
2024-03-04 21:35:46 +0100

    add comment

--- src/main/java/hu/webler/OopApp.java
+++ src/main/java/hu/webler/OopApp.java
@@ -4,8 +4,19 @@ import hu.webler.model.Car;
 
 public class OopApp {
 
+    /*
+    Az osztály példányosítása az osztály egy konkrét objektumának létrehozását jelenti a programban. Az osztály egy sablon
+    vagy útmutató az objektum létrehozásához, és az osztály példányosításával valójában egy új objektumot hozunk létre
+    az adott osztály szerint.
+    Az osztály példányosítása során a new kulcsszó segítségével hozunk létre egy új objektumot az adott osztály alapján.
+    A konstruktor pedig az objektum inicializálásáért felelős, és a példányosítás során fut le.
+    Konstruktor lehet üres, vagy paraméteres!
+    Ha van paraméteres, akkor a default (alapértelmezett üres konstruktor nincs -> ha szükséges, létre kell hozni!)
+    */
+
     public static void main(String[] args) {
 
+        // Osztály példányositása!
         Car car = new Car();
         System.out.println(car); // amig nem volt toString -> hu.webler.model.Car@5b480cf9
         Car opel = new Car("Opel");
@@ -19,4 +30,29 @@ public class OopApp {
         opel.setSpeed(160);
         System.out.println(opel);
     }
+
+    /*
+    Az objektumorientált programozásban (OOP) az osztályok az objektumok sablonjai vagy formális leírásai. Az osztályok
+    meghatározzák az objektumok tulajdonságait (állapotát) és viselkedését (metódusokat).
+
+    Az osztályoknak két fő összetevőjük van:
+
+    1. Állapot (State) vagy Tulajdonságok (Properties):
+    Ez az osztály által tartalmazott adatokat vagy adattagokat jelenti. Ezek az adatok meghatározzák az objektum aktuális
+    állapotát vagy tulajdonságait. Például egy autó osztály esetében az állapotot a márka, sebesség, üzemanyagszint stb.
+    jelentik.
+
+    2. Viselkedés (Behavior) vagy Metódusok (Methods):
+    Ezek az osztályban definiált függvények vagy műveletek, amelyeket az osztályban létrehozott objektumok végrehajtanak.
+    Ezek a metódusok az osztály adattagjain dolgoznak, és manipulálják azokat. Például egy autó osztályban a gyorsítás,
+    fékezés, üzemanyag-fogyasztás stb. lehetnek metódusok.
+
+    Az osztályok definíciói alapján a programozók létrehozhatnak objektumokat, amelyek az adott osztály sablonjai szerint
+    működnek. Az objektumok az osztályok példányai, és az osztályokban meghatározott tulajdonságok és metódusok révén
+    képesek tárolni az állapotukat és végrehajtani a viselkedésüket.
+
+    Az osztályok tulajdonképpen az objektumorientált programozás alapvető építőkövei, és segítségükkel a programozók képesek
+    absztrakt módon modellezni a valós világot és strukturálni a programjukat úgy, hogy az könnyen érthető és karbantartható
+    legyen.
+    */
 }
